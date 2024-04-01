fa195a877e364339fecc35e25895aee460854ea5
NelHim <n.himbaza@alustudent.com>
2024-04-01 20:53:35 +0200

    feat: create dev branch

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <h1>Nelson Himbaza</h1>
+      <h1>Nelson Himbaza working from the dev branch</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
